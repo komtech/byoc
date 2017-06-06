efba3e2f7db488fee2a445f183e80641b05b3675
Sopheara Seng <soseng@live.com>
2017-06-05 21:57:33 -0700

    Test CI/CD and fix issues

--- src/main/java/com/starbucks/services/CustomerService.java
+++ src/main/java/com/starbucks/services/CustomerService.java
@@ -32,7 +32,7 @@ public class CustomerService {
 	@Path("{cust_name}") //http://localhost:8080/byoc/customers/1234
 	public Customer getCustomer(@PathParam ("cust_name") String custName)
 	{	
-    	Customer customer=repository.findByCustName("Sopheara");
+    	Customer customer=repository.findByCustName(custName);
         return customer; 
 	}
 }
